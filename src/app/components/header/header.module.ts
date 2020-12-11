2b738e3d34a63cd8e68bb00ca9a902792198953d
Eriscot <alexsun123777@gmail.com>
2020-12-11 12:33:34 +0300

    feat: header component

---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import {ButtonModule} from '../../controls/button/button.module';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  exports: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    ButtonModule
+  ]
+})
+export class HeaderModule { }
